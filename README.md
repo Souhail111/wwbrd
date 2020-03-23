d5d31af9369f2b0b343711b97f06f3b3abece7d1
Souhail111 <62572439+Souhail111@users.noreply.github.com>
2020-03-23 22:48:06 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# wwbrd
+# idkbroadcast
---
+++ bot.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setGame('w!b (Message) - w&w 🎆',"https://www.twitch.tv/peery13");
+});
+
+
+client.on('message', message => { // هاذا للبرودكسات
+        var prefix = '!'; // هنا تقدر تغير البرفكس
+	var command = message.content.split(" ")[0];
+	if(command == prefix + 'bc') { // الكوماند !bc
+		if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don`t have **ADMINISTRATOR** permission!");
+		var args = message.content.split(' ').slice(1).join(' ');
+		if(message.author.bot) return;
+		if(!args) return message.channel.send(`**➥ Useage:** ${prefix}bc كلامك`);
+		if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don`t have **ADMINISTRATOR** permission!");
+		
+		let bcSure = new Discord.RichEmbed()
+		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
+		.setThumbnail(client.user.avatarURL)
+		.setColor('#4510a8')
+		.setDescription(`**\n:envelope: ➥ رسالتك**\n\n${args}`)
+		.setTimestamp()
+		.setFooter(message.author.tag, message.author.avatarURL)
+		
+		message.channel.send(bcSure).then(msg => {
+			msg.react('✅').then(() => msg.react('❎'));
+			message.delete();
+			
+			
+			let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+			let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+			
+			let sendBC = msg.createReactionCollector(yesEmoji);
+			let dontSendBC = msg.createReactionCollector(noEmoji);
+			
+			sendBC.on('collect', r => {
+				        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .setColor('#4510a8')
+       .setTitle('Broadcast')
+       .addField('سيرفر', message.guild.name)
+       .addField('المرسل', message.author.username)
+       .addField('الرسالة',args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter('🖤 w&w is Life 🖤 ', message.guild.iconURL);
+            m.send(`${m}`,{embed: bc});
+        });
+				message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
+				msg.delete();
+			})
+			dontSendBC.on('collect', r => {
+				msg.delete();
+				message.reply(':white_check_mark: **تم الغاء ارسال رسالتك بنجاح**').then(msg => msg.delete(5000));
+			});
+		})
+	}
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN);
